78820f665cb3a038578a06c949d1c7dae74688c6
Luca Maltagliati <malta95@hotmail.it>
2021-08-12 11:57:39 +0200

    feat: upgrade firebase cloud functions to v3

--- lib/firebase_cloud_functions_mock.dart
+++ lib/firebase_cloud_functions_mock.dart
@@ -1,25 +1,33 @@
 library firebase_cloud_functions_mock;
 
-import 'package:cloud_functions/cloud_functions.dart';
-import 'package:mockito/mockito.dart';
 import 'dart:convert';
 
-class MockCloudFunctions extends Mock implements CloudFunctions {
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:mockito/mockito.dart';
+
+class MockCloudFunctions extends Mock implements FirebaseFunctions {
   Map<String, String> _jsonStore = <String, String>{};
 
-  String _convertMapToJson(Map<String, dynamic> parameters){
+  String _convertMapToJson(Map<String, dynamic> parameters) {
     return json.encode(parameters);
   }
 
-  void mockResult({String functionName, String json, dynamic parameters}){
-    functionName = parameters?.isNotEmpty ? functionName + _convertMapToJson(parameters) : functionName;
+  void mockResult({String functionName, String json, dynamic parameters}) {
+    functionName = parameters?.isNotEmpty ?? false
+        ? functionName + _convertMapToJson(parameters)
+        : functionName;
     _jsonStore[functionName] = json;
   }
 
-  String getMockResult(String functionName, dynamic parameters){
-    parameters = Map<String, dynamic>.from(parameters);
-    functionName = parameters == null ? functionName : (parameters?.isNotEmpty ? functionName + _convertMapToJson(parameters) : functionName);
-    assert(_jsonStore[functionName] != null, 'No mock result for ${functionName}');
+  String getMockResult(String functionName, dynamic parameters) {
+    parameters = Map<String, dynamic>.from(parameters ?? {});
+    functionName = parameters == null
+        ? functionName
+        : (parameters?.isNotEmpty ?? false
+            ? functionName + _convertMapToJson(parameters)
+            : functionName);
+    assert(
+        _jsonStore[functionName] != null, 'No mock result for $functionName');
     return _jsonStore[functionName];
   }
 
@@ -30,29 +38,31 @@ class MockCloudFunctions extends Mock implements CloudFunctions {
 }
 
 class HttpsCallableMock extends Mock implements HttpsCallable {
-
   HttpsCallableMock._(this._cloudFunctions, this._functionName);
 
   final MockCloudFunctions _cloudFunctions;
   final String _functionName;
 
+  // Future<HttpsCallableResult<T>> call<T>([dynamic parameters]) async {
+  //   assert(_debugIsValidParameterType(parameters));
+  //   return HttpsCallableResult<T>._(await delegate.call(parameters));
+  // }
 
   @override
-  Future<HttpsCallableResult> call([dynamic parameters]) {
-    final decoded = json.decode(_cloudFunctions.getMockResult(_functionName, parameters));
+  Future<HttpsCallableResult<T>> call<T>([dynamic parameters]) {
+    final decoded =
+        json.decode(_cloudFunctions.getMockResult(_functionName, parameters));
     return Future.value(HttpsCallableResultMock._(decoded));
   }
-
-
 
   /// The timeout to use when calling the function. Defaults to 60 seconds.
   Duration timeout;
 }
 
-class HttpsCallableResultMock extends Mock implements HttpsCallableResult{
-  HttpsCallableResultMock._(dynamic _data):
-        data = _data;
+class HttpsCallableResultMock<T> extends Mock
+    implements HttpsCallableResult<T> {
+  HttpsCallableResultMock._(T _data) : data = _data;
 
   /// Returns the data that was returned from the Callable HTTPS trigger.
-  final dynamic data;
+  final T data;
 }
